fc75f5f865b67a4291012a38148c458a25ca5ebe
AbhipsaMohapatra <abhipsamohapatra270805@gmail.com>
2026-02-14 14:31:39 +0530

    Added Contact Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,22 +19,35 @@ const Contact = () => {
   return (
     <section className="contact">
       <div className="bg-gradient-to-r from-pink-600 to bg-purple-600 via-fuchsia-300 flex justify-between items-center p-20">
-        <div className="border shadow-amber-100">
+        <div className="border border-black shadow-amber-100 p-2">
           <img
             src="contactpic3.jpg"
-            className="w-100 h-100 object-fit-cover "
+            className="w-90 h-90 object-fit-contain animate-pulse ease-in-out"
             alt="Contact"
           />
         </div>
-        <div className="border">
-          <h2>Get In Touch</h2>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-              {...register("firstName", { required: true, maxLength: 20 })}
-            />
-            <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-            <input type="number" {...register("age", { min: 18, max: 99 })} />
-            <input type="submit" />
+        <div className=" w-1/2">
+          <h2 className="text-slate-800 font-bold text-3xl">Get In Touch</h2>
+          <form onSubmit={handleSubmit(onSubmit)} className="">
+            <div className=" mt-5 mb-2 flex  items-center gap-3">
+              <input 
+                placeholder="First Name"
+                className=" text-black w-1/3 p-3 rounded-md border border-slate-800 bg-white/10 backdrop-blur-md   outline-none focus:ring-2 focus:ring-white/10 transition-all"
+                {...register("firstName", { required: true, maxLength: 20 })}
+              />
+              <input placeholder="Contact Number" className=" text-black w-1/3 p-3 rounded-md border border-slate-800 bg-white/10 backdrop-blur-md   outline-none focus:ring-2 focus:ring-white/10 transition-all" {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+            </div>
+            <div className=" mt-5 mb-2 flex  items-center gap-3">
+
+            <input className=" text-black w-1/3 p-3 rounded-md border border-slate-800 bg-white/10 backdrop-blur-md   outline-none focus:ring-2 focus:ring-white/10 transition-all" type="number" {...register("age", { min: 18, max: 99 })} />
+            <input className=" text-black w-1/3 p-3 rounded-md border border-slate-800 bg-white/10 backdrop-blur-md   outline-none focus:ring-2 focus:ring-white/10 transition-all" type="number" {...register("age", { min: 18, max: 99 })} />
+            </div>
+            <div className="my-2">
+              <textarea placeholder="Enter Your Message" className=" text-black w-2/3 p-3 rounded-md border border-slate-800 bg-white/10 backdrop-blur-md   outline-none focus:ring-2 focus:ring-white/10 transition-all">
+
+              </textarea>
+            </div>
+            <input className=" text-black w-1/3 p-3 rounded-md border border-slate-800 bg-white/10 backdrop-blur-md   outline-none focus:ring-2 focus:ring-white/10 transition-all" type="submit" />
           </form>
         </div>
       </div>
